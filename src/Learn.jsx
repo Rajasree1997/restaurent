47a4411fc7ec09a8f30bcd1924bbcfdc57cda532
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-11-18 14:35:43 +0530

    18/11/second

--- src/Learn.jsx
+++ src/Learn.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Learn = () => {
+    const [todoList,setTodoList]=useState([]);
+    const [todo,setTodo]=useState("")
     return (
+        
         <div>
-            <h1>Learn</h1>
+        <h2>My To Do List</h2>
+        <div className="todo-add">
+            <input type="text"  placeholder="Add new..." value={todo} onChange={(e)=>{
+                setTodo(e.target.value);//todo il type cheyyunna value settodo lekk store aavunnu when click on add new botton
+             }}/>
+        
+        <button onClick={()=>{ 
+            setTodoList(prev=>[...prev,todo]);
+            setTodo("")//add cheyda sesham type cheyyunna place empty aavan
+
+        } }> ADD NEW</button>
+        <div className="todo-content">
+
+        <ul>
+        {todoList.map((value,i)=><li key={i}
+            onClick={()=>{
+                setTodoList(todoList.filter((_value,index)=>i!==index
+                )
+                )
+            }}>
+                {value}</li>)}
+        </ul>
         </div>
+        </div>
+        </div>
+      
     )
 }
 
