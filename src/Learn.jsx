1b340bc75af98a8a13290fc0002748c97e41f891
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-11-19 13:54:14 +0530

    18/11/third

--- src/Learn.jsx
+++ src/Learn.jsx
@@ -8,12 +8,12 @@ const Learn = () => {
         <div className=" container">
         <h2>My To Do List</h2>
         <div className="todo-add">
-            <input type="text"  placeholder="Add new..." value={todo} onChange={(e)=>{
+            <input type="text"  placeholder="Plan Something..." value={todo} onChange={(e)=>{
                 setTodo(e.target.value);//todo il type cheyyunna value settodo lekk store aavunnu when click on add new botton
              }}/>
         
         <button className="todo-button" onClick={()=>{
-            if(!todo)return 
+            if(!todo)return //if else can also use
             setTodoList(prev=>[...prev,{text:todo,status:false}]);//previously ulla list settpdplistlekk vakkunnu plus puthiyathum add aavunnu
             setTodo("")//add cheyda sesham type cheyyunna place empty aavan
 
