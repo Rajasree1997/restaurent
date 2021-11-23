38ebd046a35702f6a1d3b55a9295deeb200aa4ce
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-11-23 11:23:32 +0530

    23/11/second

--- src/JS/Async.jsx
+++ src/JS/Async.jsx
@@ -1,24 +1,44 @@
 import { useEffect } from "react"
 const Async = () => {
-    useEffect(()=>{
-        const firstFunction = (a,b,next)=>{
-            let c;
-            setTimeout(()=>{
-                c =a+ "&" +b;
-                next(c);
+    useEffect(async()=>{
+        // const firstFunction = (a,b,next)=>{
+        //     let c;
+        //     setTimeout(()=>{
+        //         c =a+ "&" +b;
+        //         next(c);
 
-            },500);
+        //     },500);
 
-        }
+        // }
         
                 
-        firstFunction("apple","orange",(result)=>{
-            console.log(result);
-        });
-               
-    
+        // firstFunction("apple","orange",(result)=>{
+        //     console.log(result);
+        // });
+        const myPromise =(a,b)=>{
+         return new Promise((resolve,reject)=>
+        {
+            setTimeout(()=>{
+                        
+                         let c =a + "&" + b;
+                    //  resolve(c);
+                        reject("error");
+                    },500);
+        }
+        )}
 
-    },[])
+        //   myPromise("hello","there").then((result)=>{
+        //       console.log(result);
+        //   }).catch(result=>{console.log("catch",result);
+
+        //   })
+        try{     
+                const result = await myPromise("HELLO","THERE");
+                console.log(result);
+
+        }catch(error){
+            console.log("catch",error);
+        }},[])
     return (
         <div>
             <h1>Promises,Async and await</h1>
